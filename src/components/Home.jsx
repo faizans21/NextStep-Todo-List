6714c6796b86abbdd027bef3079304779ef2c6fe
faizans21 <faizans2049@gmail.com>
2025-07-19 22:38:36 +0530

    localstorage added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { v4 as uuidv4 } from 'uuid';
 
@@ -6,13 +6,35 @@ const Home = () => {
 
   const [todo, setTodo] = useState("")
   const [todos, setTodos] = useState([])
+  
 
-  const handleEdit = () => {
+  useEffect(() => {
+    let todoString = localStorage.getItem("todos")
+    if (todoString) {
+      let todos = JSON.parse(localStorage.getItem("todos"))
+      setTodos(todos)
+    }
+  }, [])
 
+  useEffect(() => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}, [todos]);
+
+
+  const handleEdit = (e, id) => {
+    let t = todos.filter(item => item.id === id)
+    setTodo(t[0].todo)
+    let newTodos = todos.filter(item => {
+      return item.id !== id
+    })
+    setTodos(newTodos)
   }
 
-  const handleDelete = () => {
-
+  const handleDelete = (e, id) => {
+    let newTodos = todos.filter(item => {
+      return item.id !== id
+    })
+    setTodos(newTodos)
   }
 
   const handleAdd = () => {
@@ -26,7 +48,7 @@ const Home = () => {
 
   const handleCheckbox = (e) => {
     let id = e.target.id
-    let index = todos.findIndex(item=>{
+    let index = todos.findIndex(item => {
       return item.id === id
     })
     let newTodos = [...todos]
@@ -55,18 +77,22 @@ const Home = () => {
           <h2 className='text-xl font-bold text-[#F2E9E4] mt-2 ml-2 mb-3'>Your Task's</h2>
         </div>
 
+        {todos.length === 0 && <div className='text-l font-medium mt-2 ml-5 text-[#F4EDE4]'>Nothing to do — add a task!</div>}
+
         {todos.map((ele, idx) => {
           return (
             <div key={ele.id} className="flex items-center justify-between rounded-lg py-2.5 px-4 mt-2 mx-2 bg-[#F2E9E4] ">
-              <input type="checkbox" onChange={handleCheckbox} name="" value={ele.isCompleted} id={ele.id} />
-              <div className={ele.isCompleted ? "line-through text-gray-500" : "text-[#111]"}>
-                {ele.todo}
+              <div className='flex gap-2.5'>
+                <input type="checkbox" onChange={handleCheckbox} name="" value={ele.isCompleted} id={ele.id} />
+                <div className={ele.isCompleted ? "text-[#111]" : "line-through text-gray-500"}>
+                  {ele.todo}
+                </div>
               </div>
               <div className="flex gap-2">
-                <button onClick={handleEdit} className="bg-[#4A4E69] text-white rounded">
+                <button onClick={(e) => { handleEdit(e, ele.id) }} className="bg-[#4A4E69] text-white rounded">
                   Ed
                 </button>
-                <button onClick={handleDelete} className="bg-red-500 text-white rounded">
+                <button onClick={(e) => { handleDelete(e, ele.id) }} className="bg-red-500 text-white rounded">
                   Del
                 </button>
               </div>
